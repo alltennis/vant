0e27329e54d52f400c648094bbfd42440d4cfbb6
lvdou <jack.wang@alltennis.cn>
2019-09-12 15:45:32 +0800

    adjust

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   plugins: {
-    autoprefixer: {}
+    autoprefixer: {},
+    'postcss-pxtorem': {
+        rootValue: 10,
+        minPixelValue: 2,
+        propWhiteList: []
+    }
   }
 };
